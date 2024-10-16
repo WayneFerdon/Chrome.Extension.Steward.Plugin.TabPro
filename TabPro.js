69df195fe65ab880d4ecb58a6ea333a64ea9ca34
WayneFerdon <wayneferdon@hotmail.com>
2024-10-16 15:55:15 +0800

    Update TabPro.js

--- TabPro.js
+++ TabPro.js
@@ -1,249 +1,182 @@
 module.exports = function (steward) {
-    // Keys to call the plugin
-    // Key to attach page
+    // ---------Keys of the plugin---------
     const KEY_ATTACH = 'taba';
-    // Key to detach page
     const KEY_DETACH = 'tabd';
-    // Key to focus on page
     const KEY_FOCUS = 'tabf';
 
     // ---------Text of the plugin---------
-    // Title of the plugin:
-    // 'Manage tabs and windows'
+    // before input
     const TITLE = '管理标签页与窗口';
-    // Title of tabs with unknow title (while the tab is a chrome setting page):
-    // '[Unknown Title NewTab or Chrome Steeing Pages]'
-    const UNKNOW_TAB_TITLE = '[未知标题新标签页或设置页面]';
-    // Title of attach page:
-    // 'Attach Tab'
+
     const TITLE_ATTACH = '合并标签页';
-    // Title of detach page:
-    // 'Detach and move Tab'
     const TITLE_DETACH = '移动标签页';
-    // Title of focus on page:
-    // 'Focus on Tab'
     const TITLE_FOCUS = '跳转到标签页';
-    // Title of detach to new window:
-    // 'New Window'
-    const TITLE_NEW_WINDOW = '新窗口';
-    // Title while current only exist one window:
-    // 'Only one window exist'
-    const TITLE_ONLY_ONE_WINDOW = '当前只有一个窗口'
-    // Subtitle of attach page:
-    // 'Move all tabs to current window'
-    const SUBTITLE_ATTACH = '将所有标签页合并到指定的窗口';
-    // Subtitle of attach page:
-    // 'Detach current tab from current window and attach to another window'
+    const SUBTITLE_ATTACH = '合并标签页';
     const SUBTITLE_DETACH = '查找并移动一个标签页到指定的窗口';
-    // Subtitle of attach page:
-    // 'Search and focus on a tab'
-    const SUBTITLE_FOCUS = '查找并跳转到标签页';
-    // Subtitle of detach to new window:
-    // 'Detach current tab to new window'
-    const SUBTITLE_NEW_WINDOW = '移动到新窗口';
+    const SUBTITLE_FOCUS = '跳转到标签页';
+    // on query
+    const UNKNOW_TAB_TITLE = '[未知标题新标签页或设置页面]'; // any
+
+    const TITLE_NOTHING_TO_ATTACH = '当前只有一个窗口';
+    const SUBTITLE_NOTHING_TO_ATTACH = '当前只有一个窗口';
+    const TITLE_ATTACH_ALL = '合并所有标签页';
+    const SUBTITLE_ATTACH_ALL = '将所有标签页合并到当前窗口';
+    const SUBTITLE_ATTACH_WIN = '移动标签页到当前窗口';
+    const SUBTITLE_ATTACH_TAB = '合并此窗口的所有标签页';
+
+    const TITLE_DETACH_TO_NEW = '新窗口';
+    const SUBTITLE_DETACH_TO_NEW = '移动到新窗口';
+    const SUBTITLE_DETACH_TO = '移动到此窗口';
 
     // -------Plugin Core Functions--------
-    let CurrentWindowId;
-    let CurrentTabs = [];
-    let CurrendWindow;
-    let OtherTabs = [];
-
-    function DetachSelectedTab(item) {
-        chrome.tabs.getSelected(null, tab => {
-            if (tab.id < 0) {
-                return
-            }
-            if (!item.id) {
-                chrome.windows.create({ tabId: tab.id, focused: true })
-                return
-            }
-            chrome.tabs.move(
-                tab.id,
-                { windowId: item.id, index: item.tabIndex + 1 },
-                console.log
-            )
-            UpdateTab(tab.id, { active: true }, item.id);
-        })
-    }
-
-    function GetAllTabs(query) {
-        return new Promise((resolve) => {
-            chrome.windows.getAll(
-                { populate: true },
-                function (wins) {
-                    if (!wins.length) {
-                        resolve([]);
-                        return
-                    }
-                    wins = wins.filter(win => win.id !== CurrentWindowId);
-                    const tabs = wins.reduce((memo, win) => {
-                        memo.push(...win.tabs)
-                        return memo
-                    }, [])
-                    const results = tabs.filter(function (tab) {
-                        return steward.util.matchText(query, `${tab.title}${tab.url}`);
-                    });
-                    resolve(results)
-                }
-            );
-        })
-    }
-
-    function FormatData(list) {
-        return list.map(function (item, index) {
-            // let desc = CommandFocus.subtitle;
+    function FormatData(list, command) {
+        return list.map(item => {
             return {
-                key: CommandFocus.key,
+                key: command.key,
                 id: item.id,
-                icon: item.favIconUrl || PLUGIN_ICON,
-                title: TitleFormat(item),
-                desc: CommandFocus.subtitle,
+                icon: item.favIconUrl ?? PLUGIN_ICON,
+                title: item.title ?? UNKNOW_TAB_TITLE,
+                desc: command.subtitle,
                 isWarn: item.active,
                 raw: item
+            }
+        });
+    }
+
+    /**
+     * 
+     * @param {*} method chrome.windows.{method}
+     * @returns 
+     */
+    function WindowsOnPromise(method) {
+        return new Promise(r => method({ populate: true }, w => r(w)));
+    }
+
+    async function asyncQueryTab(query, filter) {
+        let tabs = (await WindowsOnPromise(chrome.windows.getAll))
+            .reduce((memo, win) => {
+                memo.push(...win.tabs)
+                return memo;
+            }, []);
+        if (filter) tabs = tabs.filter(filter);
+        return tabs.filter(tab => {
+            return steward.util.matchText(query, `${tab.title}${tab.url}`);
+        });
+    }
+
+    async function asyncQueryWindow(query, desc) {
+        const current = (await WindowsOnPromise(chrome.windows.getCurrent)).id;
+        return (await WindowsOnPromise(chrome.windows.getAll)).filter(win => {
+            if (win.id === current) return false;
+            const tab = win.tabs.filter(tab => tab.active).pop();
+            return steward.util.matchText(query, `${tab.title}${tab.url}`)
+        }).map(win => {
+            const tab = win.tabs.filter(tab => tab.active).pop();
+            return {
+                id: win.id,
+                icon: tab.favIconUrl ?? PLUGIN_ICON,
+                title: tab.title ?? UNKNOW_TAB_TITLE,
+                desc: desc,
+                tabId: tab.id,
+                tabIndex: win.tabs.length - 1
             };
         });
     }
 
-    function TitleFormat(tab) {
-        if (tab.title == null) {
-            return UNKNOW_TAB_TITLE;
-        }
-        return tab.title;
-    }
-
-    function GetOtherWindows() {
-        return new Promise(resolve => {
-            chrome.windows.getAll({ populate: true }, wins => {
-                function getOthers() {
-                    const otherWins = wins.filter(win => win.id !== CurrendWindow.id);
-                    resolve(otherWins);
-                }
-
-                if (!wins.length) {
-                    resolve([]);
-                    return
-                }
-                CurrendWindow = wins.find(win => win.focused);
-                if (CurrendWindow) {
-                    CurrentWindowId = CurrendWindow.id;
-                    getOthers();
-                    return
-                }
-                chrome.windows.getCurrent({ populate: true }, result => {
-                    CurrendWindow = result;
-                    CurrentWindowId = CurrendWindow.id;
-                    getOthers();
-                })
-            });
-        });
-    }
-
-    function AttachTabs() {
-        chrome.windows.getAll({ populate: true }, wins => {
-            function moveTabs() {
-                let tabCount = CurrentTabs.length;
-                OtherTabs.forEach(({ id: tabId }) => {
-                    chrome.tabs.move(tabId, { windowId: CurrendWindow.id, index: tabCount++ }, console.log)
-                })
-            }
-
-            if (!wins.length) {
-                steward.util.toast(TITLE_ONLY_ONE_WINDOW);
-                return
-            }
-
-            wins.forEach(win => {
-                if (!win.focused) {
-                    OtherTabs = OtherTabs.concat(win.tabs);
-                    return
-                }
-                CurrentTabs = win.tabs;
-                CurrendWindow = win;
-            });
-
-            if (CurrendWindow) {
-                moveTabs();
-                return
-            }
-            chrome.windows.getLastFocused({ populate: true }, result => {
-                CurrendWindow = result;
-                CurrentTabs = result.tabs;
-                moveTabs();
-            })
-        });
-    }
-
-    function GetOtherWindowsResult() {
-        return GetOtherWindows().then(wins => {
-            return wins.map(win => {
-                const tab = win.tabs.pop();
-                tab.index = win.tabs.length;
-                return {
-                    id: win.id,
-                    icon: tab.favIconUrl || PLUGIN_ICON,
-                    title: TitleFormat(tab),
-                    desc: '移动到此窗口',
-                    tabId: tab.id,
-                    tabIndex: tab.index
-                }
-            });
-        });
-    }
-
-    function UpdateWindow(windowId, updateProperties) {
-        return chrome.windows.update(windowId, updateProperties);
-    }
-
     function UpdateTab(id, updateProperties, windowId) {
         if (updateProperties.active) {
-            UpdateWindow(windowId, {
-                focused: true
-            })
+            chrome.windows.update(windowId, { focused: true });
         }
         return chrome.tabs.update(id, updateProperties);
     }
 
-    function ActiveTab(item) {
-        UpdateTab(item.id, {
-            active: true
-        }, item.raw.windowId);
+    async function asyncOnAttachInput(query) {
+        const winResults = await asyncQueryWindow(query, SUBTITLE_ATTACH_TAB);
+        const current = (await WindowsOnPromise(chrome.windows.getCurrent)).id;
+        const tabs = await asyncQueryTab(query, tab => tab.windowId !== current);
+        // default: attach all
+        let results = COMMAND_ATTACH.default;
+        tabs.forEach(tab => results[0].id.push(tab.id));
+        // attach window
+        winResults.forEach(r => {
+            r.windowId = r.id;
+            r.id = [];
+        });
+        tabs.forEach(
+            tab => winResults.filter(r => r.windowId === tab.windowId).forEach(
+                r => r.id.push(tab.id)
+            )
+        );
+        winResults.forEach(r => {
+            r.title += ` [ +${r.id.length - 1} 个标签页]`;
+        });
+        results = results.concat(winResults.filter(r => r.id.length > 1));
+        // attach tab
+        tabs.forEach(tab => {
+            const tabResults = FormatData([tab], COMMAND_ATTACH);
+            tabResults.forEach(r => {
+                r.desc = SUBTITLE_ATTACH_WIN;
+                r.id = [r.id];
+            });
+            results = results.concat(tabResults);
+        });
+        if (results.length === 1) {
+            results[0].title = TITLE_NOTHING_TO_ATTACH;
+            results[0].desc = SUBTITLE_NOTHING_TO_ATTACH;
+        }
+        return results;
     }
 
-    function OnInput(query, command) {
-        switch (command.key) {
-            case KEY_ATTACH:
-                const result = steward.util.getDefaultResult(command);
-                result[0].isDefault = false;
-                return Promise.resolve(result);
-            case KEY_DETACH:
-                return GetOtherWindowsResult().then(
-                    items => {
-                        return DEFAULT_DETACH_RESULT.concat(items);
-                    });
-            case KEY_FOCUS:
-                return GetOtherWindows().then(
-                    items => GetAllTabs(query).then(tabs => {
-                        return FormatData(tabs);
-                    }))
-            default:
-                return;
-        }
+    async function asyncOnDetachInput(query) {
+        return COMMAND_DETACH.default.concat(await asyncQueryWindow(query, SUBTITLE_DETACH_TO));
     }
 
-    function OnEnter(item, command, query, shiftKey, list) {
-        switch (command.key) {
-            case KEY_ATTACH:
-                AttachTabs();
-                break;
-            case KEY_DETACH:
-                DetachSelectedTab(item);
-                break;
-            case KEY_FOCUS:
-                ActiveTab(item);
-                break;
-            default:
-                break;
+    async function asyncOnFocusInput(query) {
+        const activeTabID = (await WindowsOnPromise(chrome.windows.getCurrent)).tabs.filter(tab => tab.active).pop().id;
+        const filter = tab => tab.id !== activeTabID;
+        return FormatData(await asyncQueryTab(query, filter), COMMAND_FOCUS);
+    }
+
+    function onAttach(item) {
+        if (item.length === 1) {
+            return;
         }
+        chrome.windows.getCurrent({ populate: true }, current => {
+            item.id.forEach(tabId => {
+                chrome.tabs.move(tabId, { windowId: current.id, index: current.tabs.length + 1 }, console.log);
+            })
+            steward.app.refresh();
+        })
+    }
+
+    function onDetach(item) {
+        async function asyncMoveTabs() {
+            (await WindowsOnPromise(chrome.windows.getCurrent)).tabs.filter(tab => tab.highlighted && tab.id >= 0).forEach(tab => {
+                chrome.tabs.move(
+                    tab.id,
+                    { windowId: item.id, index: item.tabIndex++ },
+                    console.log
+                );
+                UpdateTab(tab.id, { active: true }, item.id);
+                steward.app.refresh();
+            })
+        }
+        if (item.id) {
+            asyncMoveTabs();
+            return;
+        }
+        chrome.windows.create(null, window => {
+            const holder = window.tabs[0].id;
+            item.id = window.id;
+            item.tabIndex = 0;
+            asyncMoveTabs();
+            chrome.tabs.remove(holder);
+        });
+    }
+
+    function onFocus(item) {
+        UpdateTab(item.id, { active: true }, item.raw.windowId);
+        steward.app.refresh();
     }
 
     // CONSTANTS
@@ -254,33 +187,49 @@ module.exports = function (steward) {
     const PLUGIN_CATEGORY = 'browser';
     // const PLUGIN_ICON = 'https://i.imgur.com/QcoukjA.png';
     const PLUGIN_ICON = './iconfont/tab.svg';
-    const DEFAULT_DETACH_RESULT = [
-        {
-            icon: PLUGIN_ICON,
-            title: TITLE_NEW_WINDOW,
-            desc: SUBTITLE_NEW_WINDOW
-        }
-    ];
-    const CommandAttach = {
+    const COMMAND_ATTACH = {
         key: KEY_ATTACH,
         type: PLUGIN_TYPE,
         title: TITLE_ATTACH,
         subtitle: SUBTITLE_ATTACH,
-        icon: PLUGIN_ICON
+        icon: PLUGIN_ICON,
+        onEnter: onAttach,
+        onInput: asyncOnAttachInput,
+        default: [
+            {
+                key: KEY_ATTACH,
+                icon: PLUGIN_ICON,
+                title: TITLE_ATTACH_ALL,
+                desc: SUBTITLE_ATTACH_ALL,
+                id: [],
+            }
+        ]
     }
-    const CommandDetach = {
+    const COMMAND_DETACH = {
         key: KEY_DETACH,
         type: PLUGIN_TYPE,
         title: TITLE_DETACH,
         subtitle: SUBTITLE_DETACH,
-        icon: PLUGIN_ICON
+        icon: PLUGIN_ICON,
+        onEnter: onDetach,
+        onInput: asyncOnDetachInput,
+        default: [
+            {
+                key: KEY_DETACH,
+                icon: PLUGIN_ICON,
+                title: TITLE_DETACH_TO_NEW,
+                desc: SUBTITLE_DETACH_TO_NEW
+            }
+        ]
     }
-    const CommandFocus = {
+    const COMMAND_FOCUS = {
         key: KEY_FOCUS,
         type: PLUGIN_TYPE,
         title: TITLE_FOCUS,
         subtitle: SUBTITLE_FOCUS,
-        icon: PLUGIN_ICON
+        icon: PLUGIN_ICON,
+        onEnter: onFocus,
+        onInput: asyncOnFocusInput
     }
 
     return {
@@ -290,8 +239,8 @@ module.exports = function (steward) {
         category: PLUGIN_CATEGORY,
         icon: PLUGIN_ICON,
         title: TITLE,
-        commands: [CommandAttach, CommandDetach, CommandFocus],
-        onInput: OnInput,
-        onEnter: OnEnter
+        commands: [COMMAND_ATTACH, COMMAND_DETACH, COMMAND_FOCUS],
+        onInput: (query, command) => { return command.onInput(query) },
+        onEnter: (item, command, query, { shiftKey }, list) => command.onEnter(item)
     };
 }
